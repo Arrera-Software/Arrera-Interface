b58b5f1b6e855306e8e5b6032371b6c9c313e1b9
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-01-03 13:11:07 +0100

    Creation de l'interface , ajout icon et creation des fonction

--- CArreraUI.cpp
+++ CArreraUI.cpp
@@ -13,3 +13,94 @@ CArreraUI::~CArreraUI()
     delete ui;
 }
 
+// Bouton Bottom
+void CArreraUI::on_IDC_QUIT_clicked()
+{
+    qApp->quit();
+}
+
+
+void CArreraUI::on_IDC_PARAMETRE_clicked()
+{
+
+}
+
+// Bouton assistant
+void CArreraUI::on_IDC_SIX_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_COPILOTE_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_RYLEY_clicked()
+{
+
+}
+
+// bouton mode
+
+void CArreraUI::on_IDC_MODE1_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_MODE2_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_MODE3_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_MODE4_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_MODE5_clicked()
+{
+
+}
+
+// Bouton app
+void CArreraUI::on_IDC_TABLEUR_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_TRAITEMENT_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_APP_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_NAVIGATEUR_clicked()
+{
+
+}
+
+
+void CArreraUI::on_IDC_PRESENTATION_clicked()
+{
+
+}
+
--- CArreraUI.h
+++ CArreraUI.h
@@ -15,6 +15,37 @@ public:
     CArreraUI(QWidget *parent = nullptr);
     ~CArreraUI();
 
+private slots:
+    void on_IDC_QUIT_clicked();
+
+    void on_IDC_PARAMETRE_clicked();
+
+    void on_IDC_SIX_clicked();
+
+    void on_IDC_COPILOTE_clicked();
+
+    void on_IDC_RYLEY_clicked();
+
+    void on_IDC_MODE1_clicked();
+
+    void on_IDC_MODE2_clicked();
+
+    void on_IDC_MODE3_clicked();
+
+    void on_IDC_MODE4_clicked();
+
+    void on_IDC_MODE5_clicked();
+
+    void on_IDC_TABLEUR_clicked();
+
+    void on_IDC_TRAITEMENT_clicked();
+
+    void on_IDC_APP_clicked();
+
+    void on_IDC_NAVIGATEUR_clicked();
+
+    void on_IDC_PRESENTATION_clicked();
+
 private:
     Ui::CArreraUI *ui;
 };
