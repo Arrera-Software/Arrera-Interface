4607fc8ed86bee44e52ea42e79dd5b31b780fc82
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-01-16 16:12:19 +0100

    Ajout fnc  a chaque bouton + transition entre les deux affichage

--- arreraapplib.cpp
+++ arreraapplib.cpp
@@ -6,9 +6,127 @@ ArreraAppLib::ArreraAppLib(QWidget *parent)
     , ui(new Ui::ArreraAppLib)
 {
     ui->setupUi(this);
+    ui->FARRERAAPP->setVisible(false);
+    ui->IDC_BTNAPP->setVisible(false);
+    ui->FAPP->setVisible(true);
+    ui->IDC_BTNARRERAAPP->setVisible(true);
 }
 
 ArreraAppLib::~ArreraAppLib()
 {
     delete ui;
 }
+// btn bottom bar
+void ArreraAppLib::on_IDC_BTNAPP_clicked()
+{
+    ui->FARRERAAPP->setVisible(false);
+    ui->IDC_BTNAPP->setVisible(false);
+    ui->FAPP->setVisible(true);
+    ui->IDC_BTNARRERAAPP->setVisible(true);
+}
+
+
+void ArreraAppLib::on_IDC_BTNARRERAAPP_clicked()
+{
+    ui->FARRERAAPP->setVisible(true);
+    ui->IDC_BTNAPP->setVisible(true);
+    ui->FAPP->setVisible(false);
+    ui->IDC_BTNARRERAAPP->setVisible(false);
+}
+
+// btn App
+void ArreraAppLib::on_IDC_BTNAPP1_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP2_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP3_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP4_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP5_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP6_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP7_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP8_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP9_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP10_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP11_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAPP12_clicked()
+{
+
+}
+
+//btn app arrera
+void ArreraAppLib::on_IDC_BTNAVIDEO_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNADOC_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNAINFO_clicked()
+{
+
+}
+
+
+void ArreraAppLib::on_IDC_BTNARECHERCHE_clicked()
+{
+
+}
+
--- arreraapplib.h
+++ arreraapplib.h
@@ -15,6 +15,43 @@ public:
     explicit ArreraAppLib(QWidget *parent = nullptr);
     ~ArreraAppLib();
 
+private slots:
+    void on_IDC_BTNAPP_clicked();
+
+    void on_IDC_BTNARRERAAPP_clicked();
+
+    void on_IDC_BTNAPP1_clicked();
+
+    void on_IDC_BTNAPP2_clicked();
+
+    void on_IDC_BTNAPP3_clicked();
+
+    void on_IDC_BTNAPP4_clicked();
+
+    void on_IDC_BTNAPP5_clicked();
+
+    void on_IDC_BTNAPP6_clicked();
+
+    void on_IDC_BTNAPP7_clicked();
+
+    void on_IDC_BTNAPP8_clicked();
+
+    void on_IDC_BTNAPP9_clicked();
+
+    void on_IDC_BTNAPP10_clicked();
+
+    void on_IDC_BTNAPP11_clicked();
+
+    void on_IDC_BTNAPP12_clicked();
+
+    void on_IDC_BTNAVIDEO_clicked();
+
+    void on_IDC_BTNADOC_clicked();
+
+    void on_IDC_BTNAINFO_clicked();
+
+    void on_IDC_BTNARECHERCHE_clicked();
+
 private:
     Ui::ArreraAppLib *ui;
 };
