16c891669dc58837e6777398587f672006a8467a
Baptiste P <baptiste.pauchet13@gmail.com>
2025-01-18 12:42:36 +0100

    Ajout page apropos

--- arreraui.cpp
+++ arreraui.cpp
@@ -26,7 +26,7 @@ ArreraUI::ArreraUI(QWidget *parent)
     ui->FBUREAUTIQUEAPP->setStyleSheet("#MainWidget {"
                                         "border: 2px solid black;"
                                         "border-radius: 15px;"
-                                        "}");
+                                       "}");
 
 }
 
@@ -44,6 +44,11 @@ void ArreraUI::on_IDC_ACCEUILARRERA_clicked() // Bouton Arrera en haut a gauche
         ui->I2025->setCurrentIndex(idPageI2025Main);
         ui->LINDICATIONARRERA->setText("Arrera I2025");
     }
+    else
+    {
+        ui->I2025->setCurrentIndex(idPageApropos);
+        ui->LINDICATIONARRERA->setText("Arrera I2025 A propos");
+    }
 }
 
 
@@ -59,5 +64,21 @@ void ArreraUI::on_IDC_ARRERAAPP_clicked()
 {
     ui->I2025->setCurrentIndex(idPageI2025ArreraApp);
     ui->LINDICATIONARRERA->setText("Page module Arrera");
+}
+
+
+
+// Partie Apropos
+void ArreraUI::on_IDC_SOURCECODE_clicked()
+{
+    QUrl url("https://github.com/Arrera-Software/Arrera-Interface");
+    QDesktopServices::openUrl(url);
+}
+
+
+void ArreraUI::on_IDC_WEBSITE_clicked()
+{
+    QUrl url("https://www.arrera-software.fr/");
+    QDesktopServices::openUrl(url);
 }
 
--- arreraui.h
+++ arreraui.h
@@ -4,6 +4,9 @@
 
 #include <QWidget>
 #include <QDialog>
+#include <QApplication>
+#include <QDesktopServices>
+#include <QUrl>
 
 namespace Ui {
 class ArreraUI;
@@ -24,9 +27,14 @@ private slots:
 
     void on_IDC_ARRERAAPP_clicked();
 
+    // Partie Apropos
+    void on_IDC_SOURCECODE_clicked();
+
+    void on_IDC_WEBSITE_clicked();
+
 private:
     Ui::ArreraUI *ui;
-    int idPageI2025Main,idPageI2025App,idPageI2025Mode,idPageI2025ArreraApp;
+    int idPageI2025Main,idPageI2025App,idPageI2025Mode,idPageI2025ArreraApp,idPageApropos;
 };
 
 #endif // ARRERAUI_H
