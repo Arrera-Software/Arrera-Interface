f1a7653de9e868eae3a0246c8074dab451d6a79e
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-10-29 21:00:22 +0100

    Mise en place de la version

--- version.h
+++ version.h
@@ -5,7 +5,7 @@
 
 class version{
 private :
-    const QString version = "IXXXX-XXX" ;
+    const QString version = "I2025-2.50-LTS" ;
     const QString name = "ARRERA-INTERFACE" ;
 public :
     QString getVersion(){
